df66f46870febd4fb8b2f429222e1c34a51868a6
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-09-11 20:27:00 +0100

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDl29RteKyzpJzgTsyVqvnQQ1Sw2Ia0rok",
+  authDomain: "stripe-6e2ac.firebaseapp.com",
+  databaseURL: "https://stripe-6e2ac.firebaseio.com",
+  projectId: "stripe-6e2ac",
+  storageBucket: "stripe-6e2ac.appspot.com",
+  messagingSenderId: "682609006035",
+  appId: "1:682609006035:web:d3d7350d04c14772788b26",
+  measurementId: "G-DW022GLHMW",
+};
